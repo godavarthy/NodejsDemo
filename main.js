9c8298e1db50a10f19a9c75bce30f034a6bb8d91
uditag10 <udit.agarwal1007@gmail.com>
2016-05-20 12:58:29 +0530

    handle post request and handle dir

--- main.js
+++ main.js
@@ -1,33 +1,56 @@
+/**
+ * 
+ */
 var express = require('express');
+var multer  =   require('multer');
 var http = require('http');
 var fs = require("fs");
 var unzip = require('unzip');
 var app = express();
 var port = 3000;
+var filename;
+	var storage =   multer.diskStorage({
+		  destination: function (req, file, callback) {
+		    callback(null, './inFolder');
+		  },
+		  filename: function (req, file, callback) {
+		     filename = file.fieldname + '-' + Date.now();
+		    callback(null, filename);
+		  }
+	});
+	var upload = multer({ storage : storage}).single('datafile');
 
-app.get('/demo', function (req, res) {
-  
-       console.log( "req done");
+	
+	app.post('/demo',function(req,res){
+		
+		
+	    upload(req,res,function(err) {
+		
+	        if(err) {
+	            return res.end("Error uploading file.");
+	        }
+	        else{
+			console.log( filename);
+	        	var data = "name of files are ";
+	        	var zipStream = fs.createReadStream('./inFolder/'+filename);       
+	        	zipStream.pipe(unzip.Extract({ path: './outFolder' }));
 
-var data = "name of files are ";
-       
-fs.createReadStream('C:/nodeDemo/demoFiles.zip').pipe(unzip.Extract({ path: 'C:/nodeDemo/unzipFolder' }));
+	        	var files = fs.readdirSync('./outFolder')
+	        	console.log( files);
+	        	for (var i in files){
+	        		data += files[i] + " ";
+	        		console.log( data);
+	        	}
+	        	res.end( data );
+	        }
+	    });
+	});
+	
 
-fs.createReadStream('C:/nodeDemo/demoFiles.zip')
-  .pipe(unzip.Parse())
-  .on('entry', function (entry) {
-    var fileName = entry.path;
-	console.log( fileName);
-    var type = entry.type; // 'Directory' or 'File' 
-    var size = entry.size;
-	data += fileName +" ";
-console.log( data );
-    
-  });
-res.end( data );
-  
+app.set('view engine', 'jade');
+app.get('/index', function(req, res) {
+    res.sendfile('test.html', {root: './views' })
 });
-
 
 var server = http.createServer(app).listen(process.env.PORT || port, function () {
 
